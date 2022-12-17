dbee88c674f5ad9e19089bb02bbde11e2484cd1e
Drewthurm21 <drewthurm21@gmail.com>
2022-12-17 08:59:16 -0600

    added more solutions to product except self - js

--- arrays/jsSolutions/06-productOfArrayExceptSelf-medium.js
+++ arrays/jsSolutions/06-productOfArrayExceptSelf-medium.js
@@ -1,10 +1,9 @@
 /*
 Given an integer array nums, return an array answer such that answer[i] is equal to the 
-product of all the elements of nums except nums[i]. The product of any prefix or suffix 
+product of all the elements of nums except nums[i]. The product of any prefix or suffix
 of nums is guaranteed to fit in a 32-bit integer.
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
-
  
 Example 1:
 Input: nums = [1,2,3,4]
@@ -29,38 +28,78 @@ const productOfArrayExceptSelf = (nums) => {
 
   for (let i = 0; i < nums.length; i++) {
     let product = 1
+
     for (let j = 0; j < nums.length; j++) {
       if (i !== j) {
         product *= nums[j]
       }
     }
-    result.push(product)
-  }
-
-  return result
-}
-
-// easier to read
-const productExceptSelf = (nums) => {
-  let result = []
-
-  for (const excludedIndex in nums) {
-    let product = 1
-
-    for (const currentIndex in nums) {
-      if (currentIndex === excludedIndex) continue;
-      else product *= nums[currentIndex]
-    }
 
     result.push(product)
   }
 
   return result
 }
-
 
 // console.log(productOfArrayExceptSelf([1, 2, 3, 4]))
-// console.log(productOfArrayExceptSelf([-1, 1, 0, -3, 3]))
+// console.log(productOfArrayExceptSelf([-1, 1, 0, -3, 3])
 
-console.log(productExceptSelf([1, 2, 3, 4]))
-console.log(productExceptSelf([-1, 1, 0, -3, 3]))
+/* 
+[1,1,2,6] - 1
+[1,2,3,4]
+
+[24,12,4,1]
+*/
+
+//refactor using arrays - O(2n) space
+const prodExceptSelf = (nums) => {
+  let n = nums.length
+  let prev = new Array(n)
+  let post = new Array(n)
+  let prod = new Array(n)
+
+  prev[0] = 1
+  post[n - 1] = 1
+
+  //iterate over nums to fill the prev products array
+  for (let i = 1; i < nums.length; i++) {
+    prev[i] = nums[i - 1] * prev[i - 1]
+  }
+
+  //iterate over nums backward to fill the post products array
+  for (let i = nums.length - 2; i >= 0; i--) {
+    post[i] = nums[i + 1] * post[i + 1]
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    prod[i] = post[i] * prev[i]
+  }
+
+  return prod
+}
+
+// console.log(prodExceptSelf([1, 2, 3, 4]))
+
+
+//refactor using one array - O(n) space
+const prodXeptSelf = (nums) => {
+  const res = [];
+  let prod = 1;
+
+  // iterate over nums
+  // use prod as a product accumulator
+  // push current prefix product into res
+  for (let k of nums) res.push(prod *= k);
+
+  //iterate backwards over nums
+  //use prod as a product accumulator, multiplying by nums[i]
+  //prod should hold postfix from end to start+1
+  for (let prod = 1, i = nums.length - 1; i >= 0; prod *= nums[i--]) {
+    //assign res[i] to current product * trailing res index
+    //use accumulated prod for last index (res[0]) 
+    res[i] = i ? prod * res[i - 1] : prod;
+  }
+
+  return res;
+}
+console.log(prodXeptSelf([1, 2, 3, 4]))
