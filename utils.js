e5eea542835011ab669ddb3b11b5f353798b2df2
Drewthurm21 <drewthurm21@gmail.com>
2023-01-20 16:05:51 -0600

    added simple class sorter

--- utils.js
+++ utils.js
@@ -11,6 +11,18 @@ const timer = (testFunc, numTimes, ...vals) => {
 }
 
 
+const classSorter = (list) => {
+  const teams = { 0: [], 1: [], 2: [], 3: [], 4: [] }
+  for (let i in list) teams[i % 5].push(list[i])
+  console.log(teams)
+}
+
+const students = [
+  'student names here'
+]
+
+classSorter(students)
+
 module.exports = {
   timer
 }
