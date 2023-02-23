102d8ad0c3b3a6914c7df9b9124c0f99d1bd8d87
Drewthurm21 <drewthurm21@gmail.com>
2023-02-22 18:13:31 -0600

    added string prob

---
+++ strings/pySolutions/robot_tracker.py
@@ -0,0 +1,30 @@
+'''
+A robot has been given a list of movement instructions. Each instruction 
+is either left, right, up or down, followed by a distance to move. 
+The robot starts at [0, 0]. You want to calculate where the robot will 
+end up and return its final position as a list. 
+
+For example, if the robot is given the instructions: 
+["right 10", "up 50", "left 30", "down 10"]
+
+it will end its path 20 left and 40 up from where it started, so you 
+should return [-20, 40].
+'''
+
+
+def robot_tracker(path):
+    loc = {
+        'up': 0,
+        'down': 0,
+        'left': 0,
+        'right': 0
+    }
+
+    for instruction in path:
+        direction, distance = tuple(instruction.split(' '))
+        loc[direction] = int(distance)
+
+    return [loc['left'] - loc['right'], loc['up'] - loc['down']]
+
+
+print(robot_tracker(["right 10", "up 50", "left 30", "down 10"]))
