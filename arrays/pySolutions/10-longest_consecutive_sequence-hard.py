0d7a110aa6e45487c5dec141b7416eb6e647c114
Drewthurm21 <drewthurm21@gmail.com>
2023-03-26 12:49:53 -0500

    added longest consecutive sequence for js with comments

--- arrays/pySolutions/10-longest_consecutive_sequence-hard.py
+++ arrays/pySolutions/10-longest_consecutive_sequence-hard.py
@@ -19,12 +19,12 @@ def find_longest_sequence(nums):
     for num in nums:
         length = 0
         if num - 1 not in nums_set:
-            while num in nums_set:
+            while num + length in nums_set:
                 length += 1
-                num += 1
-                longest = length
+            longest = max([length, longest])
     return longest
 
 
 print(find_longest_sequence([100, 4, 200, 1, 3, 2]))  # 4
-print(find_longest_sequence([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))  # 9
+print(find_longest_sequence(
+    [0, 3, 7, 2, 5, 8, 4, 6, 0, 1, 201, 202, 203]))  # 9
