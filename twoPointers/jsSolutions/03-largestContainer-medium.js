bc518f7e39a9f8c326cca2f255e3c1d76ac0896f
Drewthurm21 <drewthurm21@gmail.com>
2023-03-27 16:46:19 -0500

    added trapping rain water problem - js/py - solved js

--- twoPointers/jsSolutions/03-largestContainer-medium.js
+++ twoPointers/jsSolutions/03-largestContainer-medium.js
@@ -1,4 +1,4 @@
-`
+/*
 You are given an integer array height of length n. There are n 
 vertical lines drawn such that the two endpoints of the ith line are 
 (i, 0) and (i, height[i]). Find two lines that together with the x-axis 
@@ -18,7 +18,7 @@ contain is 49.
 Example 2:
 Input: height = [1,1]
 Output: 1
-`
+*/
 
 const largestContainer = (heights) => {
   let maxL = 0, L = 0, maxR = 0, R = heights.length - 1, maxArea = 0;
