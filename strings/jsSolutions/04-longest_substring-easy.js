add1be5e3a7489c840eaa4d27eefa10f725e8afb
Drewthurm21 <drewthurm21@gmail.com>
2024-01-04 01:13:28 -0600

    added solution for longest substring in js

---
+++ strings/jsSolutions/04-longest_substring-easy.js
@@ -0,0 +1,39 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+*/
+
+const longestCommonPrefix = (strs) => {
+  let pre = ''
+  let len = strs[0].length
+
+  for (let i = 0; i < len; i++) {
+    let char = strs[0][i]
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) return pre
+    }
+    pre += char
+  }
+
+  return pre
+}
